b58931d222c1d7dc0fd62070a18f6c649663d524
Sarveshwar Hirve <sarveshwar@gmail.com>
2026-04-09 20:36:09 +0530

    Updated - Key VaultX API progress

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from auth import generate_api_key
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return{"message":"API is working"}
+
+@app.post ("/generate-key")
+def create_key():
+    api_key = generate_api_key()
+    return{
+        "api_key":api_key, "status":"created"
+    }
